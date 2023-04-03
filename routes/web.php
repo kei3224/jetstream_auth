9cfab48cfa84d7f184aa16ab8a751767723d4d61
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2023-04-03 15:57:56 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'checkUserType']);
+
+Route::get('/admin/dashboard', function(){
+    return view('admin-dashboard');
+})->name('admin.dashboard');
+
+Route::get('/user/dashboard', function(){
+    return view('user-dashboard');
+})->name('user.dashboard');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
